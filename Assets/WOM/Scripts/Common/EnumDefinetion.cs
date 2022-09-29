5f8aa60798d2b224daec48f5aa12b44ea36d585d
freeboxwork <33273908+freeboxwork@users.noreply.github.com>
2022-09-29 17:43:45 +0900

    model data 추가
    model data 추가

---
+++ Assets/WOM/Scripts/Common/EnumDefinetion.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnumDefinetion 
+{
+    public enum InsectType
+    {
+        mentis,
+        bee,
+        beetle
+    }
+    public enum MonsterType
+    {
+        normal,
+        gold,
+        boss
+    }
+}
