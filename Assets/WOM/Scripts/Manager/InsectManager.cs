2984fc8f329a6391d7c430fa94f135719dd41824
freeboxwork <33273908+freeboxwork@users.noreply.github.com>
2022-10-03 22:06:58 +0900

    insect, monster manager 로직 추가
    insect, monster manager 로직 추가

---
+++ Assets/WOM/Scripts/Manager/InsectManager.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InsectManager : MonoBehaviour
+{
+    public InsectBee prefabInsectBee;
+    public InsectBeetle prefabInsectBeetle;
+    public InsectMentis prefabInsectMentis;
+
+    void Start()
+    {
+        
+    }
+
+    void InitancingInsects()
+    {
+
+    }
+  
+}
