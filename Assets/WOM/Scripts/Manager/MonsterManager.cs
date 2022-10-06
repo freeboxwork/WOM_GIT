80885a74229d60af4772958ac0d046ad29930d4e
freeboxwork <33273908+freeboxwork@users.noreply.github.com>
2022-10-06 21:00:00 +0900

    몬스터 로드 로직 추가

--- Assets/WOM/Scripts/Manager/MonsterManager.cs
+++ Assets/WOM/Scripts/Manager/MonsterManager.cs
@@ -16,5 +16,61 @@ public class MonsterManager : MonoBehaviour
 
     }
 
+
+    public IEnumerator Init(StageData stageData)
+    {
+        yield return null;
+
+        var monNormalData = GetMonsterData(EnumDefinetion.MonsterType.normal, stageData.monsterNormalId);
+        var monGoldData = GetMonsterData(EnumDefinetion.MonsterType.gold, stageData.monsterGoldId);
+        var monBossData = GetMonsterData(EnumDefinetion.MonsterType.normal, stageData.monsterNormalId);
+
+        SetMonsterData(EnumDefinetion.MonsterType.normal, monNormalData);
+        SetMonsterData(EnumDefinetion.MonsterType.gold, monGoldData);
+        SetMonsterData(EnumDefinetion.MonsterType.boss, monBossData);
+    }
+
+    MonsterData GetMonsterData(EnumDefinetion.MonsterType monsterType, int monsterId)
+    {
+        var data = GlobalData.instance.dataManager.GetMonsterDataById(monsterType,monsterId); 
+        if(data == null)
+        {
+            //TODO : ERROR PRINT 관리자 만들기
+            Debug.LogError("");
+            return null;
+        }
+        else
+        {
+            return data;    
+        }
+    }
+
+    void SetMonsterData(EnumDefinetion.MonsterType monsterType , MonsterData monsterData)
+    {
+        var monster = GetMonsterData(monsterType);
+        
+        monster.monsterId = monsterData.monsterId;
+        monster.hp = monsterData.hp;
+        monster.exp = monsterData.exp;
+        monster.gold = monsterData.gold;
+        monster.goldCount = monsterData.goldCount;
+        monster.imageId = monsterData.imageId;
+        monster.bgId = monsterData.bgId;
+        monster.monsterType = monsterData.monsterType;
+        monster.attackType = monsterData.attackType;
+    }
+
+
+
+    MonsterBase GetMonsterData(EnumDefinetion.MonsterType monsterType)
+    {
+        switch (monsterType)
+        {
+            case EnumDefinetion.MonsterType.normal: return monsterNormal;
+            case EnumDefinetion.MonsterType.gold: return monsterGold;
+            case EnumDefinetion.MonsterType.boss: return monsterBold;   
+        }
+        return null;
+    }
    
 }
