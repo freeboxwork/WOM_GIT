653dada6cf0bf505c591b17fed302e92b46e69a0
freeboxwork <33273908+freeboxwork@users.noreply.github.com>
2022-10-15 14:32:01 +0900

    모스 몬스터 피격 쉐이더 이펙트 로직 추가
    모스 몬스터 피격 쉐이더 이펙트 로직 추가

--- Assets/WOM/Scripts/Controller/MonsterInOutAnimator.cs
+++ Assets/WOM/Scripts/Controller/MonsterInOutAnimator.cs
@@ -7,6 +7,7 @@ public class MonsterInOutAnimator : MonoBehaviour
 {
     public AnimData animDataIn;
     public AnimData animDataDissolve;
+    public AnimData animDataHit;
     public Material dissolveMat;
     public Transform startPoint_MonsterIn;
     public Transform endPoint_MonsterIn;
@@ -71,6 +72,52 @@ public class MonsterInOutAnimator : MonoBehaviour
     public void MonsterKillAnimWithOutEvent()
     {
         StartCoroutine(MonsterDieMatAnim(dissolveMat, "_fade", (1, 0), false));
+    }
+
+    public IEnumerator MonsterKillMatAnim()
+    {
+        // collider disable
+        monsterCol.enabled = false;
+
+        isAnimPlay = true;
+        animDataDissolve.ResetAnimData();
+        while (animDataDissolve.animTime < 0.999f)
+        {
+            animDataDissolve.animTime = (Time.time - animDataDissolve.animStartTime) / animDataDissolve.animDuration;
+            //Debug.Log(animDataDissolve.animTime);
+            animDataDissolve.animValue = EaseValues.instance.GetAnimCurve(animDataDissolve.animCurveType, animDataDissolve.animTime);
+            var value = Mathf.Lerp(1f, 0f, animDataDissolve.animValue);
+            dissolveMat.SetFloat("_fade", value);
+            yield return null;
+        }
+        isAnimPlay = false;
+        transform.position = startPoint_MonsterIn.position;
+
+        // TODO : EVENT CONTROLLER 에서 코루틴으로 제어
+        
+        //if (runKillEvnet)
+        //    EventManager.instance.RunEvent<EnumDefinition.MonsterType>(CallBackEventType.TYPES.OnMonsterKill, monster.monsterType);
+
+    }
+    public void MonsterHitAnim()
+    {
+        StartCoroutine(MonsterHitMatAnim(dissolveMat, "_hit", (0.4f, 0)));
+    }
+         
+
+    public IEnumerator MonsterHitMatAnim(Material mat, string property, (float, float) minMax)
+    {
+        // collider disable
+        animDataHit.ResetAnimData();
+        while (animDataHit.animTime < 0.999f)
+        {
+            animDataHit.animTime = (Time.time - animDataHit.animStartTime) / animDataHit.animDuration;
+            animDataHit.animValue = EaseValues.instance.GetAnimCurve(animDataHit.animCurveType, animDataHit.animTime);
+            var value = Mathf.Lerp(minMax.Item1, minMax.Item2, animDataHit.animValue);
+            mat.SetFloat(property, value);
+            yield return null;
+        }
+        
     }
 
 
