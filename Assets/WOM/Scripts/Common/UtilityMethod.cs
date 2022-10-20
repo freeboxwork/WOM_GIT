1151c318766e3538afb65f1f074f3879d4afa76f
freeboxwork <33273908+freeboxwork@users.noreply.github.com>
2022-10-20 09:16:35 +0900

    골드 관련 로직 추가 ( 작업중 )
    골드 관련 로직 추가 ( 작업중 이니 다운받지 마세용~ )

--- Assets/WOM/Scripts/Common/UtilityMethod.cs
+++ Assets/WOM/Scripts/Common/UtilityMethod.cs
@@ -25,4 +25,20 @@ public static class UtilityMethod
         }
     }
 
+    public static float GetRandomRangeValue_X(Vector2 point, float rv)
+    {
+        return Random.Range(point.x - rv, point.x + rv);
+    }
+    public static float GetRandomRangeValue_Y(Vector2 point, float rv)
+    {
+        return Random.Range(point.y - rv, point.y + rv);
+    }
+
+    public static Vector2 GetTargetRandomRangeValue_V2(Vector2 x_min, Vector2 x_max, Vector2 y_min, Vector2 y_max)
+    {
+        var x = Random.Range(x_min.x, x_max.x);
+        var y = Random.Range(y_min.y, y_max.y);
+        return new Vector2(x, y);
+    }
+
 }
---
+++ Assets/WOM/Scripts/FX/GoldAnimController.cs
@@ -0,0 +1,134 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.VFX;
+
+public class GoldAnimController : MonoBehaviour
+{
+    public AnimData goldInAnimData;
+    public AnimData goldOutAnimData;
+
+    // RV : RANDOM VALUE
+    float startPointRV = 1f;
+
+    float randomRotMin = 1f;
+    float randomRotMax = 720f;
+    float randomRotValue;
+    Vector3 startRotValue;
+    Vector3 targetRotValue;
+    Quaternion startRot;
+    Quaternion targetRot;
+
+    Vector2 pos_startPoint;
+    Vector2 pos_targetPoint;
+    float rot_randomValue;
+
+
+
+    void Start()
+    {
+
+    }
+
+    private void OnEnable()
+    {
+        //SetRandomRandomPositions();
+        //SetRandomRotValue();
+        //StartCoroutine(GoldAnim());
+    }
+
+    private void Update()
+    {
+        //if (Input.GetKeyDown(KeyCode.K))
+        //{
+        //    StartCoroutine(GoldOutAnim());
+        //}
+    }
+
+    public void GoldOutAnimStart()
+    {
+        StartCoroutine(GoldOutAnim());
+    }
+
+
+    void SetRandomRotValue()
+    {
+        startRotValue = Vector3.zero;
+        randomRotValue = Random.Range(randomRotMin, randomRotMax);
+        targetRotValue = new Vector3(0, 0, randomRotValue);
+
+        startRot = Quaternion.Euler(startRotValue);
+        targetRot = Quaternion.Euler(targetRotValue);
+    }
+
+    void SetRandomRandomPositions()
+    {
+        var startPoint = GlobalData.instance.effectManager.GetGoldSfxRandomPoint(EnumDefinition.GoldPosType.START_POINT_Y).position;
+        var xMin = GlobalData.instance.effectManager.GetGoldSfxRandomPoint(EnumDefinition.GoldPosType.END_POOINT_X_MIN).position;
+        var xMax = GlobalData.instance.effectManager.GetGoldSfxRandomPoint(EnumDefinition.GoldPosType.END_POOINT_X_MAX).position;
+        var yMin = GlobalData.instance.effectManager.GetGoldSfxRandomPoint(EnumDefinition.GoldPosType.END_POOINT_Y_MIN).position;
+        var yMax = GlobalData.instance.effectManager.GetGoldSfxRandomPoint(EnumDefinition.GoldPosType.END_POOINT_Y_MAX).position;
+
+        var startX = UtilityMethod.GetRandomRangeValue_X(startPoint, startPointRV);
+        var startY = GlobalData.instance.effectManager.GetGoldSfxRandomPoint(EnumDefinition.GoldPosType.START_POINT_Y).position.y;
+        pos_startPoint = new Vector2(startX, startY);
+        pos_targetPoint = UtilityMethod.GetTargetRandomRangeValue_V2(xMin, xMax, yMin, yMax);
+    }
+
+
+
+    IEnumerator GoldAnim()
+    {
+        goldInAnimData.ResetAnimData();
+
+        while (goldInAnimData.animValue < 0.99f)
+        {
+            goldInAnimData.animTime = ((Time.time - goldInAnimData.animStartTime) / goldInAnimData.animDuration);
+            goldInAnimData.animValue = EaseValues.instance.EaseOutQuint(0f, 1f, goldInAnimData.animTime);
+            transform.position = Vector2.Lerp(pos_startPoint, pos_targetPoint, goldInAnimData.animValue);
+            transform.rotation = Quaternion.Lerp(startRot, targetRot, goldInAnimData.animValue);
+
+            yield return null;
+        }
+    }
+
+
+    IEnumerator GoldOutAnim()
+    {
+        goldOutAnimData.ResetAnimData();
+        var curPos = transform.position;
+        var uiPoint = GlobalData.instance.effectManager.GetGoldSfxRandomPoint(EnumDefinition.GoldPosType.SCREEN_UI_POINT);
+        var targetPos = Camera.main.ScreenToWorldPoint(uiPoint.position);
+        var centerPos = (curPos + targetPos) * 0.5f;
+        float centerRandomX = Random.Range(-2f, 2f);
+        centerPos = new Vector2(centerPos.x + centerRandomX, centerPos.y);
+
+        while (goldOutAnimData.animValue < 0.99f)
+        {
+            goldOutAnimData.animTime = ((Time.time - goldOutAnimData.animStartTime) / goldOutAnimData.animDuration);
+            goldOutAnimData.animValue = EaseValues.instance.EaseInQuad(0f, 1f, goldOutAnimData.animTime);
+
+            transform.position = CalculateBezierPoint(goldOutAnimData.animValue, curPos, centerPos, targetPos);
+            //transform.position = Vector2.Lerp(curPos, targetPos, goldOutAnimData.animValue);
+            transform.rotation = Quaternion.Lerp(targetRot, startRot, goldOutAnimData.animValue);
+            yield return null;
+        }
+
+        gameObject.SetActive(false);
+    }
+
+
+
+
+    Vector2 CalculateBezierPoint(float t, Vector2 p0, Vector2 p1, Vector2 p2)
+    {
+        var a = Vector2.Lerp(p0, p1, t);
+        var b = Vector2.Lerp(p1, p2, t);
+        var c = Vector2.Lerp(a, b, t);
+        var d = Vector2.Lerp(b, c, t);
+        var e = Vector2.Lerp(c, d, t);
+
+        return e;
+    }
+
+}
