c081f2f31ef1c35b92633841be48a77069bf4c6b
centuruk <63578897+centuruk@users.noreply.github.com>
2022-11-07 21:05:32 +0900

    유니온 스프라이트 데이터 최종 정리 및 리스트업.

--- Assets/WOM/Scripts/Graphics/G_Editor/SpriteFileDataEditor.cs
+++ Assets/WOM/Scripts/Graphics/G_Editor/SpriteFileDataEditor.cs
@@ -1,42 +1,8 @@
+using System.Text.RegularExpressions;
 using UnityEditor;
 using UnityEngine;
 
 #if UNITY_EDITOR
-/*
-[CustomEditor(typeof(SpriteFileData))]
-public class SpriteDataListInspector : Editor
-{
-    ReorderableList lists;
-
-    private void OnEnable()
-    {
-        var prop = serializedObject.FindProperty("spriteDatas");
-        lists = new ReorderableList(serializedObject, prop);
-
-        //엘러먼트 크기
-        lists.elementHeight = 80;
-
-        //헤더
-        lists.drawHeaderCallback = (rect) => EditorGUI.LabelField(rect, "spriteDatas");
-
-        //엘러먼트 연결
-        lists.drawElementCallback = (rect, index, isActive, isFocused) =>
-        {
-            var element = prop.GetArrayElementAtIndex(index);
-            EditorGUI.PropertyField(rect, element);
-        };
-    }
-
-
-    public override void OnInspectorGUI()
-    {
-        //base.OnInspectorGUI();
-        serializedObject.Update();
-        lists.DoLayoutList();
-        serializedObject.ApplyModifiedProperties();
-    }
-}
-*/
 [CustomPropertyDrawer(typeof(SpriteFileData.SpriteDataInfo))]
 public class SpriteDataInfoDraw : PropertyDrawer
 {
@@ -45,7 +11,7 @@ public class SpriteDataInfoDraw : PropertyDrawer
     public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
     {
         //base.OnGUI(position, property, label);
-
+        
         using (new EditorGUI.PropertyScope(position, label, property))
         {
             EditorGUIUtility.labelWidth = 50;
@@ -69,20 +35,23 @@ public class SpriteDataInfoDraw : PropertyDrawer
             spriteProperty.objectReferenceValue =
                 EditorGUI.ObjectField(spriteRect, spriteProperty.objectReferenceValue, typeof(Sprite), false);
             EditorGUI.LabelField(new Rect(position.x + 75, position.y + 40, 64, 64), "sprite");
-            
-            //라벨
-            EditorGUI.LabelField(new Rect(position.x + 150, position.y, position.width * 0.2f, position.height),
-                                "Number");
 
-            //데이터 넘버
-            var numberRect = new Rect(position.x + 220, position.y, 30, position.height);
-            var numberProperty = property.FindPropertyRelative("num");
-            EditorGUI.PropertyField(numberRect, numberProperty, GUIContent.none);
-            
+            //라벨 (정보표시)
+            string indexNumber = property.displayName;
+            indexNumber = Regex.Replace(indexNumber, @"\D", "");
+            string spriteName = "";
+            if(property.FindPropertyRelative("sprite").objectReferenceValue != null)
+            {
+                spriteName = property.FindPropertyRelative("sprite").objectReferenceValue.name;
+            }
+
+            EditorGUI.LabelField(new Rect(position.x + 150, position.y, position.width * 0.5f, position.height),
+                                "Number : " + indexNumber + " , " + spriteName);
+
             //라벨
             EditorGUI.LabelField(new Rect(position.x + 150, position.y + (position.height + 2), position.width * 0.2f, position.height),
                                 "Discription");
-            
+
             //설명
             var discriptionRect =
                 new Rect(position.x + 220, position.y + (position.height + 2), position.width * 0.3f, position.height);
@@ -93,8 +62,6 @@ public class SpriteDataInfoDraw : PropertyDrawer
             var enumRect = new Rect(position.x + 150, position.y +((position.height*2) + 10) , halfWidth, position.height);
             var enumPropoerty = property.FindPropertyRelative("type");
             EditorGUI.PropertyField(enumRect, enumPropoerty, GUIContent.none);
-
-
         }
     }
 
