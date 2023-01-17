0174ec8ba63e1470a81005db2d0365d5a3b15770
freeboxwork <33273908+freeboxwork@users.noreply.github.com>
2023-01-17 15:08:49 +0900

    유니온 info popup ui 구성
    유니온 info popup ui 구성

---
+++ Assets/WOM/Scripts/Controller/UnionInfoPopupController.cs
@@ -0,0 +1,110 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class UnionInfoPopupController : MonoBehaviour
+{
+    public GameObject popup;
+    public Image imgUnionFace;
+    public TextMeshProUGUI txtUinonName;
+    public TextMeshProUGUI txtUinonGrade;
+    public TextMeshProUGUI txtDamage;
+    public TextMeshProUGUI txtSpawnTime;
+    public TextMeshProUGUI txtMoveSpeed;
+    public TextMeshProUGUI txtPassiveDamage;
+    public TextMeshProUGUI txtReqirementCount;
+    public Slider slider;
+    public Button btnEquip;
+    public Button btnLevelUp;
+    public Button btnClose;
+    UnionInGameData unionInGameData;
+
+    void Start()
+    {
+        
+    }
+
+    void SetBtnEvent()
+    {
+        btnEquip.onClick.AddListener(() =>
+        {
+
+        });
+
+        btnLevelUp.onClick.AddListener(() =>
+        {
+
+        });
+
+        btnClose.onClick.AddListener(() =>
+        {
+            popup.SetActive(false); 
+        });
+    }
+    public void EnablePopup(UnionSlot slot, UnionData data, ref UnionInGameData inGameData)
+    {
+        // SET UI
+        SetImgFace(slot.imgUnionFace.sprite);
+        SetTxtUnionName(data.name);
+        SetTxtUinonGrade(data.gradeName);
+
+        unionInGameData = inGameData;
+
+        // SET STAT UI
+        SetTxtDamage(inGameData.damage.ToString());
+        SetTxtSpawnTime(inGameData.spawnTime.ToString());
+        SetTxtMoveSpeed(inGameData.moveSpeed.ToString());
+        SetTxtPassiveDamage(inGameData.passiveDamage.ToString());
+        SetSlider(slot.sliderReqirement.value);
+        SetTxtReqirementCount(slot.txtReqirementCount.text);
+    }
+  
+    public void SetTxtUnionName(string value)
+    {
+        txtUinonName.text = value;
+    }
+
+    public void SetTxtUinonGrade(string value)
+    {
+        txtUinonGrade.text = value;
+    }
+
+    public void SetTxtDamage(string value)
+    {
+        txtDamage.text = value; 
+    }
+
+    public void SetTxtSpawnTime(string value)
+    {
+        txtSpawnTime.text = value;  
+    }
+
+    public void SetTxtMoveSpeed(string value)
+    {
+        txtMoveSpeed.text = value;
+    }
+
+    public void SetTxtPassiveDamage(string value)
+    {
+        txtPassiveDamage.text = value;
+    }
+
+    public void SetTxtReqirementCount(string value)
+    {
+        txtReqirementCount.text = value;
+    }
+
+    public void SetImgFace(Sprite sprite)
+    {
+        imgUnionFace.sprite = sprite;
+    }
+   
+    public void SetSlider(float value)
+    {
+        slider.value = value;
+    }
+
+
+}
