1b1eeca92925a5b2423879aff19d689f3c6857c5
freeboxwork <33273908+freeboxwork@users.noreply.github.com>
2023-02-02 15:17:10 +0900

    수정 사항 커밋 ( 작업중 )
    수정 사항 커밋 ( 작업중 )

---
+++ Assets/WOM/Scripts/Common/ShopSlot.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:c1e46a3299df16efbdedd62cf48188ae599ff5159e21f90dc087f1d7b864e239
+size 586
---
+++ Assets/WOM/Scripts/Manager/ShopManager.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:6149dc39b26e0471da93a405257296a778156a5f550f45e6fba12b8a78e04c76
+size 2316
