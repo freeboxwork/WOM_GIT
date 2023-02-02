40571851165f355f710b1b51b89da7e6aa8f47bd
centuruk <63578897+centuruk@users.noreply.github.com>
2023-02-02 15:37:41 +0900

    Revert "Revert "옴마,, 다시 해보기""
    This reverts commit 3d866ffc05db2d7bc0c35cff19e9b3e007d1cdb4.

--- Assets/WOM/Scripts/Data/CustomTypeDataManager.cs
+++ Assets/WOM/Scripts/Data/CustomTypeDataManager.cs
@@ -1,3 +1,126 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:d440fead1db16be978dcc8b6310b2af3b021000443c79448ed18762f0cd279f7
-size 4746
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+using UnityEngine.UI;
+using TMPro;
+using UnityEditor.PackageManager;
+
+public class CustomTypeDataManager : MonoBehaviour
+{
+    
+
+    public List<CustomTypeData> customTypeDatas = new List<CustomTypeData>();
+
+    // TODO : 타입별로 셋팅 진행
+    public List<CustomTypeData> customTypeDatas_animCont = new List<CustomTypeData>();
+    public List<CustomTypeData> customTypeDatas_uiButton = new List<CustomTypeData>();
+    public List<CustomTypeData> customTypeDatas_uiText = new List<CustomTypeData>();
+    public List<CustomTypeData> customTypeDatas_uiImage = new List<CustomTypeData>();
+    public List<CustomTypeData> customTypeDatas_transform = new List<CustomTypeData>();
+    public List<CustomTypeData> customTypeDatas_gameObject = new List<CustomTypeData>();
+
+    void Start()
+    {
+        
+    }
+
+
+    public CustomTypeData GetCustomTypeData(EnumDefinition.CustomDataType customType, int index)
+    {
+        var data = customTypeDatas?.FirstOrDefault(f => f.customType == customType && f.index == index);
+        if (data != null)
+            return data;
+        else
+            Debug.LogError($"{customType}_{index} custom type data 가 없습니다..");
+        return null;
+    }
+
+    public List<CustomTypeData> GetCustomTypeDatas(EnumDefinition.CustomDataType dataType)
+    {
+        switch (dataType)
+        {
+            case EnumDefinition.CustomDataType.animCont: return customTypeDatas_animCont;
+            case EnumDefinition.CustomDataType.button: return customTypeDatas_uiButton;
+            case EnumDefinition.CustomDataType.text: return customTypeDatas_uiText;
+            case EnumDefinition.CustomDataType.image: return customTypeDatas_uiImage;
+            case EnumDefinition.CustomDataType.tr: return customTypeDatas_transform;
+            case EnumDefinition.CustomDataType.gm: return customTypeDatas_gameObject;
+            default: return new List<CustomTypeData>();
+        }
+    }
+
+    public Button GetCustomTypeData_Button(int index)
+    {
+        //Debug.Log(index);
+        var data = customTypeDatas_uiButton?.FirstOrDefault(f=> f.index == index).components.button;
+        if (data != null)
+            return data;
+        else
+            Debug.LogError($"{EnumDefinition.CustomDataType.button}_{index} custom type data 가 없습니다.");
+        return null;
+    }
+
+    public TextMeshProUGUI GetCustomTypeData_Text(int index)
+    {
+        TextMeshProUGUI data = null;
+        try
+        {
+            data = customTypeDatas_uiText.FirstOrDefault(f => f.index == index).components.text;
+        }
+        catch(System.Exception e)
+        {
+            Debug.Log(e);
+            Debug.LogError($"커스텀 데이터 {EnumDefinition.CustomDataType.text} 타입의 인덱스 넘버 : {index} 가 없습니다.");
+        }
+        return data;
+        
+        //if (data != null)
+        //    return data;
+        //else
+        //    Debug.LogError($"{EnumDefinition.CustomDataType.text}_{index} custom type data 가 없습니다.");
+        //return null;
+    }
+
+    public Image GetCustomTypeData_Image(int index)
+    {
+        var data = customTypeDatas_uiImage?.FirstOrDefault(f => f.index == index).components.image;
+        if (data != null)
+            return data;
+        else
+            Debug.LogError($"{EnumDefinition.CustomDataType.image}_{index} custom type data 가 없습니다.");
+        return null;
+    }
+
+    public Transform GetCustomTypeData_Transform(int index)
+    {
+        var data = customTypeDatas_transform?.FirstOrDefault(f => f.index == index).components.tr;
+        if (data != null)
+            return data;
+        else
+            Debug.LogError($"{EnumDefinition.CustomDataType.tr}_{index} custom type data 가 없습니다.");
+        return null;
+    }
+
+    public GameObject GetCustomTypeData_Gameobject(int index)
+    {
+        var data = customTypeDatas_gameObject?.FirstOrDefault(f => f.index == index).components.gm;
+        if (data != null)
+            return data;
+        else
+            Debug.LogError($"{EnumDefinition.CustomDataType.tr}_{index} custom type data 가 없습니다.");
+        return null;
+    }
+
+
+    public AnimationController GetCustomTypeData_AnimCont(int index)
+    {
+        var data = customTypeDatas_animCont?.FirstOrDefault(f => f.index == index).components.animController;
+        if (data != null)
+            return data;
+        else
+            Debug.LogError($"{EnumDefinition.CustomDataType.animCont}_{index}  custom type data 가 없습니다.");
+        return null;
+    }
+   
+}
--- Assets/WOM/Scripts/Manager/SaleManager.cs
+++ Assets/WOM/Scripts/Manager/SaleManager.cs
@@ -1,3 +1,194 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:78aef5823732283dceedcc45012baf6dd5e1fbe74936831caea2f72978d73686
-size 6046
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using static EnumDefinition;
+
+public class SaleManager : MonoBehaviour
+{
+    Player player;
+    TraningManager traningManager;
+    Queue<SaleStatMsgData> saleStatMsgs = new Queue<SaleStatMsgData>();
+
+    void Start()
+    {
+        GetPlayer();
+        StartCoroutine( SandMessage());
+    }
+   
+    void GetPlayer()
+    {
+        player = GlobalData.instance.player;
+        traningManager = GlobalData.instance.traningManager;
+    }
+
+    public void AddData(SaleStatMsgData data)
+    {
+        saleStatMsgs.Enqueue(data);
+    }
+        
+
+    IEnumerator SandMessage()
+    {
+        while (true)
+        {
+            if(saleStatMsgs.Count > 0)
+            {
+                var data = saleStatMsgs.Dequeue();
+                yield return StartCoroutine(PurchaseStatByType(data.saleStatType));
+            }
+            yield return null;
+        }
+    }
+
+    
+    public IEnumerator PurchaseStatByType(SaleStatType statType)
+    {
+        // get current level & next statData by statType
+        var payData = GetPayData(statType);
+
+        // 최대 레벨 체크
+        if (payData.isValidMaximumLevel)
+        {
+            //재화 : GOLD
+            if (IsGoldItem(statType))
+            {
+                // 재화 충분 한지 판단
+                if (IsValidPurchaseGold(payData.statData.salePrice))
+                {
+                    // 구매 
+                    player.PayGold(payData.statData.salePrice);
+
+                    // 데이터 적용
+                    traningManager.SetInGameStatLevel(statType, payData.statData.level);
+
+                    // UI 적용
+                    if (payData.nextStatSaleData != null)
+                    {
+                        //GlobalData.instance.uiController.SetTxtTraningValues(statType, new float[] { payData.statData.value, payData.nextStatSaleData.value });
+                        //Debug.Log($"{statType}의 현재 레벨 스탯값 : { payData.statData.value} - 다음 레벨 스탯값 : {payData.nextStatSaleData.value} ");
+                        traningManager.SetUI_TraningSlot(statType);
+                    }
+                    else // 최종레벨 도달
+                    {
+                        Debug.Log("최종레벨 도달");
+                    }
+                    
+
+                }
+                else
+                {
+                    // 골드가 부족 합니다.
+                    Debug.Log($"{statType} - 골드가 부족 합니다.");
+                }
+            }
+            //재화 : BONE
+            else
+            {
+                if (IsValidPurchaseBone(payData.statData.salePrice))
+                {
+                    // 구매
+                    player.PayBone(payData.statData.salePrice);
+
+                    // 데이터 적용
+                    traningManager.SetInGameStatLevel(statType, payData.statData.level);
+
+                    // UI 적용
+                    if (payData.nextStatSaleData != null)
+                    {
+                        //GlobalData.instance.uiController.SetTxtTraningValues(statType, new float[] { payData.statData.value, payData.nextStatSaleData.value });
+                        //Debug.Log($"{statType}의 다음 레벨 : {payData.nextStatSaleData.level} , 다음 레벨 스탯값 : {payData.nextStatSaleData.value} ");
+                        traningManager.SetUI_TraningSlot(statType);
+                    }
+                    else // 최종레벨 도달
+                    {
+                        Debug.Log("최종레벨 도달");
+                    }
+                }
+                else
+                {
+                    // 뼈 조각이 부족 합니다.
+                    Debug.Log($"{statType} - 뼈 조각이 부족합니다.");
+                }
+            }
+        }
+        else
+        {
+            // 최대 레벨 도달
+            Debug.Log($"{statType} - 최대 레벨에 도달했습니다..");
+        }
+
+        yield return null;
+    }
+
+    (int level , StatSaleData statData, bool isValidMaximumLevel, StatSaleData nextStatSaleData) GetPayData( SaleStatType statType)
+    {
+        (int m_level, StatSaleData m_statData,bool m_isValidMaxLevel, StatSaleData m_nextStatData) items;
+        var statDatas = GlobalData.instance.dataManager.GetSaleStatDataByType(statType);
+        items.m_level = traningManager.GetInGameStatLevel(statType);
+    
+        // 맥시멈 체크
+        var lastData = statDatas.data.Last();
+        items.m_isValidMaxLevel =  lastData.level > items.m_level;
+        if (items.m_isValidMaxLevel)
+        {
+            items.m_statData = GetStatSaleData(statDatas, items.m_level + 1);
+        }
+        else
+        {
+            items.m_statData = null;
+        }
+
+        
+
+        // 다음 레벨 데이터 존재 하는지 체크
+        if(lastData.level > items.m_level+2)
+        {
+            items.m_nextStatData = GetStatSaleData(statDatas, items.m_level + 2);
+        }
+        else
+        {
+            items.m_nextStatData = null;
+        }
+        
+        return items;
+    }
+
+
+    StatSaleData GetStatSaleData(StatSaleDatas data, int level)
+    {
+        return data.data.FirstOrDefault(f => f.level == level);
+    }
+
+    bool IsGoldItem(SaleStatType saleStat)
+    {
+        return (saleStat == SaleStatType.trainingDamage || saleStat == SaleStatType.trainingCriticalChance || saleStat == SaleStatType.trainingCriticalDamage);
+    }
+
+
+    
+
+
+
+    //UTILITY 
+
+    bool IsValidPurchaseGold(int value)
+    {
+        return 0 <= player.gold - value;
+    }
+
+
+    bool IsValidPurchaseBone(int value)
+    {
+        return 0 <= player.bone - value;
+    }
+
+
+    bool IsValidMaximumLevel(SaleStatType statType ,int curLevel)
+    {
+        int maxumLevel = 100;
+        return maxumLevel > curLevel;
+    }
+    
+
+}
