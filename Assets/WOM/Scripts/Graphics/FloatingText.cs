acff509f986e76b44f4f452eef6d3c23de8de0f2
centuruk <63578897+centuruk@users.noreply.github.com>
2023-02-18 18:51:59 +0900

    캔버스 소팅오더 변경

--- Assets/WOM/Scripts/Graphics/FloatingText.cs
+++ Assets/WOM/Scripts/Graphics/FloatingText.cs
@@ -4,17 +4,19 @@ using UnityEngine;
 
 using DG.Tweening;
 using TMPro;
-using UnityEngine.Playables;
 
 namespace ProjectGraphics
 {
     public class FloatingText : MonoBehaviour
     {
+        [SerializeField]Transform textTransform;
         MeshRenderer rend;
         TextMeshPro textMesh;
 
         [Header("애니메이션 길이")]
         public float duration = 1.0f;
+        [Header("목표 Y 위치")]
+        public float targetPosY = 0.5f;
         [Header("글자크기")]
         public int fontsSize = 4;
 
@@ -23,12 +25,12 @@ namespace ProjectGraphics
         
         //노말 텍스트 컬러, 크리티컬 텍스트 컬러,
         //크리티컬 글자!
-
+        private Color setColor = Color.white;
 
         private void Awake()
         {
-            rend = GetComponent<MeshRenderer>();
-            textMesh = GetComponent<TextMeshPro>();
+            rend = GetComponentInChildren<MeshRenderer>();
+            textMesh = GetComponentInChildren<TextMeshPro>();
 
             //폰트 형태 변경
             //textMesh.fontStyle = FontStyles.Normal;
@@ -36,7 +38,7 @@ namespace ProjectGraphics
 
         private void OnEnable()
         {
-            
+            StartAnimation();
         }
 
         public void SetText(string text)
@@ -48,11 +50,14 @@ namespace ProjectGraphics
         {
             //위로 올라가는 애니메이션.
             //끝나면 위치 초기화
+            textTransform.DOLocalMoveY(targetPosY, duration).SetEase(Ease.OutCirc).OnComplete(() => this.gameObject.SetActive(false));
+            
         }
 
         private void OnDisable()
         {
-            
+            textTransform.localPosition = Vector3.zero;
+            textMesh.color = Color.white;
         }
     }
 }
