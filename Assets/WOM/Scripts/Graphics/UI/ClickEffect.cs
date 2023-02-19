12c76a5ebf8432aeb45a0ea872942130e3b4d320
centuruk <63578897+centuruk@users.noreply.github.com>
2023-02-19 16:55:22 +0900

    슬롯이펙트 추가

---
+++ Assets/WOM/Scripts/Graphics/UI/ClickEffect.cs
@@ -0,0 +1,74 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using UnityEngine.UI;
+using TMPro;
+
+namespace ProjectGraphics
+{
+    public class ClickEffect : MonoBehaviour
+    {
+        [Header("온오프 오브젝트")]
+        public GameObject slotBack;
+        public Image slotImage;
+        [Header("AdditiveColor")]
+        public Gradient gradientColor;
+        public AnimationCurve curve;
+        [Header("ActionText")]
+        public TextMeshProUGUI[] actionTexts;
+        private float[] baseFontSizes;
+
+        public float duration = 1.0f;
+
+        WaitForEndOfFrame waitFrame = new WaitForEndOfFrame();
+
+        private void Awake()
+        {
+            baseFontSizes = new float[actionTexts.Length];
+            for (int i = 0; i < actionTexts.Length; i++)
+            {
+                baseFontSizes[i] = actionTexts[i].fontSize;
+            }
+        }
+
+        public void OnClickButtons()
+        {
+            StartCoroutine(ClickEffectProcess());
+        }
+
+        IEnumerator ClickEffectProcess()
+        {
+            slotBack.SetActive(true);
+
+            yield return null;
+            float t = 0;
+            float clampValue = 0;
+
+            while (clampValue <= 1)
+            {
+                t += Time.deltaTime;
+                clampValue = t / duration;
+
+                slotImage.color = gradientColor.Evaluate(clampValue);
+                float upsize = curve.Evaluate(clampValue) + 1;
+
+                for (int i = 0; i < actionTexts.Length; i++)
+                {
+                    actionTexts[i].fontSize = baseFontSizes[i] * upsize;
+                }
+
+                yield return waitFrame;
+            }
+
+            yield return null;
+
+            //Return
+            for (int i = 0; i < actionTexts.Length; i++)
+            {
+                actionTexts[i].fontSize = baseFontSizes[i];
+            }
+            slotBack.SetActive(false);
+        }
+    }
+}
