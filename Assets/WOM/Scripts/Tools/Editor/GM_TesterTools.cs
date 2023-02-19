53be702f88eb3127c588e9436c6b0dbc39100e41
freeboxwork <33273908+freeboxwork@users.noreply.github.com>
2023-02-19 14:13:23 +0900

    Update GM_TesterTools.cs
    몬스터 즉시 사냥 버그 수정

--- Assets/WOM/Scripts/Tools/Editor/GM_TesterTools.cs
+++ Assets/WOM/Scripts/Tools/Editor/GM_TesterTools.cs
@@ -6,11 +6,23 @@ using static EnumDefinition;
 
 public class GM_TesterTools : EditorWindow
 {
+    Transform t;
+
     [MenuItem("GM_TOOLS/TesterTools")]
     public static void ShowWindwo()
     {
         var window = GetWindow<GM_TesterTools>();
         window.Show();
+    }
+
+    private void OnEnable()
+    {
+        var tg = GameObject.Find("TesterTransform");
+        if (tg == null)
+        {
+            tg = new GameObject("TesterTransform");
+        }
+        t = tg.transform;
     }
 
     private void OnGUI()
@@ -24,12 +36,13 @@ public class GM_TesterTools : EditorWindow
         EditorCustomGUI.GUI_Button("몬스터 즉시 사냥", () => { KillMonster(); });
     }
 
+   
 
     void KillMonster()
     {
         var player = GlobalData.instance.player;
         player.currentMonster.hp = 0;
-        EventManager.instance.RunEvent(CallBackEventType.TYPES.OnMonsterHit, EnumDefinition.InsectType.bee,0);
+        EventManager.instance.RunEvent(CallBackEventType.TYPES.OnMonsterHit, EnumDefinition.InsectType.bee,0, t);
     }
 
 }
