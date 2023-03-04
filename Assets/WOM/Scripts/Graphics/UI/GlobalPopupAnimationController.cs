3503ea5898a26da952b26d3cf35f50c091948d8d
centuruk <63578897+centuruk@users.noreply.github.com>
2023-03-04 20:53:05 +0900

    글로벌 팝업 애니메이션. 시스템(하드코딩)

--- Assets/WOM/Scripts/Graphics/UI/GlobalPopupAnimationController.cs
+++ Assets/WOM/Scripts/Graphics/UI/GlobalPopupAnimationController.cs
@@ -2,17 +2,83 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class GlobalPopupAnimationController : MonoBehaviour
+namespace ProjectGraphics
 {
-    // Start is called before the first frame update
-    void Start()
+    public class GlobalPopupAnimationController : MonoBehaviour
     {
-        
-    }
+        [Header("등장 속도")]
+        public float openDuration = 0.5f;
+        [Header("올라가는 속도")]
+        public float climbDuration = 0.5f;
+        public Vector3 climeTargetPosition;
+        public Vector3 startPosition;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        public MessageSlot[] slots;
+        [SerializeField] int currentIndex;
+
+        bool allSlotHide = true; //예외처리용
+
+        void Start()
+        {
+            currentIndex = 0;
+        }
+
+        void Update()
+        {
+            #if UNITY_EDITOR
+            //키를 눌러 다음 것이 잘 나오는지 확인 순차적으로
+            if (Input.GetKeyUp(KeyCode.Y))
+            {
+                OpenThePopup("테스트", "테스트");
+            }
+            #endif
+        }
+
+        public void OpenThePopup(string title, string message)
+        {
+            //내 자신이 안켜져 있을땐 실행 안됨.
+            if (gameObject.activeSelf == false) return;
+
+            //처음열었을때 0번 인덱스 호출하고 인덱스 입력
+            //현재 인덱스가 열려 있을때 다음 인덱스 호출.
+            //만약 현재 인덱스가 마지막 인덱스 일때 다시 0번인덱스 호출
+            //현재 인덱스는 올라가는 애니메이션 진행.
+
+            if (slots[currentIndex].gameObject.activeSelf == true) currentIndex++;
+
+            slots[currentIndex].gameObject.SetActive(true);
+            slots[currentIndex].OpenTheMessageAnimation(title, message);
+
+            int previousIndex = 0;
+
+            if (currentIndex == 0)
+            {
+                if (slots[slots.Length - 1].gameObject.activeSelf == true)
+                {
+                    slots[slots.Length - 1].ClimbTheMessageAnimation();
+                }
+                return;
+            }
+            else if (currentIndex == slots.Length - 1)
+            {
+                previousIndex = currentIndex - 1;
+                currentIndex = 0;
+            }
+            else
+            {
+                previousIndex = currentIndex - 1;
+            }
+
+            slots[previousIndex].ClimbTheMessageAnimation();
+        }
+
+        private void OnDisable()
+        {
+            currentIndex = 0;
+            for (int i = 0; i < slots.Length; i++)
+            {
+                slots[i].gameObject.SetActive(false);
+            }
+        }
     }
 }
--- Assets/WOM/Scripts/Graphics/UI/MessageSlot.cs
+++ Assets/WOM/Scripts/Graphics/UI/MessageSlot.cs
@@ -6,11 +6,75 @@ using UnityEngine.UI;
 using TMPro;
 using DG.Tweening;
 
-public class MessageSlot : MonoBehaviour
+namespace ProjectGraphics
 {
-    [SerializeField] RectTransform rect;
-    [SerializeField] TextMeshProUGUI titleText;
-    [SerializeField] RectTransform messageRect;
-    [SerializeField] TextMeshProUGUI messageText;
-    [SerializeField] RectTransform backImageRect;
+    //단순하고 심플하게
+    public class MessageSlot : MonoBehaviour
+    {
+        [SerializeField] TextMeshProUGUI titleText;
+        [SerializeField] TextMeshProUGUI messageText;
+        [SerializeField] Image backImage;
+        [SerializeField] RectTransform rect;
+        [SerializeField] GlobalPopupAnimationController popupAnimationController;
+
+        Color titleTextColor;
+        Color messageTextColor;
+        Color backImageColor;
+
+        private void Awake()
+        {
+            //준비
+            titleTextColor = titleText.color;
+            messageTextColor = messageText.color;
+            backImageColor = backImage.color;
+        }
+
+        private void OnEnable()
+        {
+            InitializedSlot();
+        }
+
+        //초기화
+        private void InitializedSlot()
+        {
+            transform.localScale = Vector3.zero;
+            rect.anchoredPosition = popupAnimationController.startPosition;
+
+            titleText.color = titleTextColor;
+            titleText.text = "Title";
+
+            messageText.color = messageTextColor;
+            messageText.text = "Message";
+
+            backImage.color = backImageColor;
+        }
+
+        //등장
+        public void OpenTheMessageAnimation(string title, string message)
+        {
+            titleText.text = title;
+            messageText.text = message;
+            transform.DOScale(Vector3.one, popupAnimationController.openDuration).SetEase(Ease.OutBack);
+        }
+
+        //밀림과 삭제
+        public void ClimbTheMessageAnimation()
+        {
+            titleText.DOFade(0.0f, popupAnimationController.climbDuration).SetEase(Ease.InQuad);
+            messageText.DOFade(0.0f, popupAnimationController.climbDuration).SetEase(Ease.InQuad);
+            backImage.DOFade(0.0f, popupAnimationController.climbDuration).SetEase(Ease.InQuad);
+            rect.DOAnchorPos(popupAnimationController.climeTargetPosition, popupAnimationController.climbDuration).SetEase(Ease.OutQuart)
+                .OnComplete(OnCompletedAnimation);
+        }
+
+        private void OnCompletedAnimation()
+        {
+            gameObject.SetActive(false);
+        }
+
+        private void OnDisable()
+        {
+            InitializedSlot();
+        }
+    }
 }
