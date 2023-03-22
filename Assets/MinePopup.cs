e3697f02112f9353e24afba7d23c47d486ee1f0a
spikedonnie <donnie127834@gmail.com>
2023-03-22 16:15:33 +0900

    무빙캐슬 팝업 업데이트

---
+++ Assets/MinePopup.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class MinePopup : MonoBehaviour
+{
+    public TextMeshProUGUI productionCountText;//생산량
+    public TextMeshProUGUI maxSupplyText;//최대저장량
+    public TextMeshProUGUI productionTimeText;//생산시간
+    public TextMeshProUGUI levelText;//레벨
+    public TextMeshProUGUI priceText;//업그레이드 석탄 비용
+
+
+    
+
+
+
+
+
+}
