2e3eb44d1f99f6c63361b4884eed2e3b6f1ec0fe
spikedonnie <donnie127834@gmail.com>
2023-03-23 23:28:00 +0900

    캐슬 컨트롤러 변수명 업데이트

--- Assets/WOM/Scripts/Graphics/Castle/CastleController.cs
+++ Assets/WOM/Scripts/Graphics/Castle/CastleController.cs
@@ -6,34 +6,34 @@ namespace ProjectGraphics
 {
     public class CastleController : MonoBehaviour
     {
-        [SerializeField] GameObject[] unionBuild;
-        [SerializeField] GameObject[] skullBuild;
-        [SerializeField] GameObject[] goldBuild;
-        [SerializeField] GameObject[] dungeonBuild;
+        [SerializeField] GameObject[] campBuild;//소환건물(캠프)
+        [SerializeField] GameObject[] factoryBuild;//뼛조각 생산건물(가공소)
+        [SerializeField] GameObject[] mineBuild;//골드 생산건물(광산)
+        [SerializeField] GameObject[] labBuild;//던전업그레이드 건물(연구소)
 
         //임시
         [Range(0, 4)]
-        public int unionBuildLv = 0;
+        public int campBuildLv = 0;
         [Range(0, 4)]
-        public int skullBuildLv = 0;
+        public int factoryBuildLv = 0;
         [Range(0, 4)]
-        public int goldBuildLv = 0;
+        public int mineBuildLv = 0;
         [Range(0, 4)]
-        public int dungeonBuildLv = 0;
+        public int labBuildLv = 0;
 
         public enum BuildingType
         {
-            UNION, SKULL, GOLD, DUNGEON
+            CAMP, FACTORY, MINE, LAB
         }
 
         void Update()
         {
-            for (int i = 0; i < unionBuild.Length; i++)
+            for (int i = 0; i < campBuild.Length; i++)
             {
-                if (unionBuildLv < i) unionBuild[i].SetActive(false);     else unionBuild[i].SetActive(true);
-                if (skullBuildLv < i) skullBuild[i].SetActive(false);     else skullBuild[i].SetActive(true);
-                if (goldBuildLv < i) goldBuild[i].SetActive(false);       else goldBuild[i].SetActive(true);
-                if (dungeonBuildLv < i) dungeonBuild[i].SetActive(false); else dungeonBuild[i].SetActive(true);
+                if (campBuildLv < i) campBuild[i].SetActive(false);     else campBuild[i].SetActive(true);
+                if (factoryBuildLv < i) factoryBuild[i].SetActive(false);     else factoryBuild[i].SetActive(true);
+                if (mineBuildLv < i) mineBuild[i].SetActive(false);       else mineBuild[i].SetActive(true);
+                if (labBuildLv < i) labBuild[i].SetActive(false); else labBuild[i].SetActive(true);
             }
         }
 
@@ -41,55 +41,55 @@ namespace ProjectGraphics
         {
             switch (type)
             {
-                case BuildingType.UNION:    SetUnionBuild(level);   break;
-                case BuildingType.SKULL:    SetSkullBuild(level);   break;
-                case BuildingType.GOLD:     SetGoldBuild(level);    break;
-                case BuildingType.DUNGEON:  SetDungeonBuild(level); break;
+                case BuildingType.CAMP: SetCampBuild(level);   break;
+                case BuildingType.FACTORY: SetFactoryBuild(level);   break;
+                case BuildingType.MINE:     SetMineBuild(level);    break;
+                case BuildingType.LAB: SetLabBuild(level); break;
             }
         }
 
-        private void SetUnionBuild(int level)
+        private void SetCampBuild(int level)
         {
-            for (int i = 0; i < unionBuild.Length; i++)
+            for (int i = 0; i < campBuild.Length; i++)
             {
-                if(i <= level) unionBuild[i].SetActive(true);
-                else unionBuild[i].SetActive(false);
+                if(i <= level) campBuild[i].SetActive(true);
+                else campBuild[i].SetActive(false);
             }
 
-            SetBuildEffect(unionBuild[level].transform.position);
+            SetBuildEffect(campBuild[level].transform.position);
         }
 
-        private void SetSkullBuild(int level)
+        private void SetFactoryBuild(int level)
         {
-            for (int i = 0; i < skullBuild.Length; i++)
+            for (int i = 0; i < factoryBuild.Length; i++)
             {
-                if (i <= level) skullBuild[i].SetActive(true);
-                else skullBuild[i].SetActive(false);
+                if (i <= level) factoryBuild[i].SetActive(true);
+                else factoryBuild[i].SetActive(false);
             }
 
-            SetBuildEffect(skullBuild[level].transform.position);
+            SetBuildEffect(factoryBuild[level].transform.position);
         }
 
-        private void SetGoldBuild(int level)
+        private void SetMineBuild(int level)
         {
-            for (int i = 0; i < goldBuild.Length; i++)
+            for (int i = 0; i < mineBuild.Length; i++)
             {
-                if (i <= level) goldBuild[i].SetActive(true);
-                else skullBuild[i].SetActive(false);
+                if (i <= level) mineBuild[i].SetActive(true);
+                else mineBuild[i].SetActive(false);
             }
 
-            SetBuildEffect(goldBuild[level].transform.position);
+            SetBuildEffect(mineBuild[level].transform.position);
         }
 
-        private void SetDungeonBuild(int level)
+        private void SetLabBuild(int level)
         {
-            for (int i = 0; i < dungeonBuild.Length; i++)
+            for (int i = 0; i < labBuild.Length; i++)
             {
-                if (i <= level) dungeonBuild[i].SetActive(true);
-                else dungeonBuild[i].SetActive(false);
+                if (i <= level) labBuild[i].SetActive(true);
+                else labBuild[i].SetActive(false);
             }
 
-            SetBuildEffect(dungeonBuild[level].transform.position);
+            SetBuildEffect(labBuild[level].transform.position);
         }
 
         private void SetBuildEffect(Vector3 pos)
