ed6c05f70fc6f5b57027c6f1f65d5ed282d58046
freeboxwork <33273908+freeboxwork@users.noreply.github.com>
2023-05-16 13:53:40 +0900

    Quest Popup Tap button group , Tap button 로직 구현
    Quest Popup Tap button group , Tap button 로직 구현

---
+++ Assets/WOM/Scripts/Controller/QuestPopup.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class QuestPopup : MonoBehaviour
+{
+    public QuestManager QuestManager;
+
+    public Button btn_close;
+    public Button btn_showListOneDay;
+    public Button btn_showListRepeat;
+    
+
+
+    void Start()
+    {
+        
+    }
+
+    void SetBtnEvents()
+    {
+
+    }
+    
+    
+}
---
+++ Assets/WOM/Scripts/Manager/QuestManager.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class QuestManager : MonoBehaviour
+{
+
+    public Button btn_showQuestPopup;
+    public QuestPopup questPopup;
+
+    
+    void Start()
+    {
+        
+    }
+
+    void SetBtnEvent()
+    {
+
+    }
+
+   
+}
