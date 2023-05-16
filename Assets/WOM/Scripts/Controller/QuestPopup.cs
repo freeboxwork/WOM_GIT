1a1f1bca920122d84c936a5ba21dbca142ca879e
freeboxwork <33273908+freeboxwork@users.noreply.github.com>
2023-05-16 14:26:39 +0900

    퀘스트 팝업 등장, 퀘스트 리스트 변경 로직 추가

--- Assets/WOM/Scripts/Controller/QuestPopup.cs
+++ Assets/WOM/Scripts/Controller/QuestPopup.cs
@@ -11,18 +11,39 @@ public class QuestPopup : MonoBehaviour
     public Button btn_close;
     public Button btn_showListOneDay;
     public Button btn_showListRepeat;
-    
+
+    public GameObject questListOneDay;
+    public GameObject questListRepeat;
+
 
 
     void Start()
     {
-        
+        SetBtnEvents();
     }
 
     void SetBtnEvents()
     {
-
+        btn_showListOneDay.onClick.AddListener(ShowQuestListOneDay);
+        btn_showListRepeat.onClick.AddListener(ShowQuestListRepeat);
+        btn_close.onClick.AddListener(ClosePopup);
     }
-    
-    
+
+    void ClosePopup()
+    {
+        QuestManager.btn_showQuestPopup.gameObject.SetActive(true);
+        gameObject.SetActive(false);
+    }
+
+    void ShowQuestListOneDay()
+    {
+        questListOneDay.SetActive(true);
+        questListRepeat.SetActive(false);
+    }
+
+    void ShowQuestListRepeat()
+    {
+        questListOneDay.SetActive(false);
+        questListRepeat.SetActive(true);
+    }
 }
